c30bea7cc29be4a2938ef21ac5640ae0333cd00e
Henry Wang <henry.wang@live.com>
2019-09-03 00:16:24 -0700

    signup functionality

--- twitter-server/handlers/auth.js
+++ twitter-server/handlers/auth.js
@@ -1,8 +1,44 @@
 const db = require('../models');
 const jwt = require('jsonwebtoken');
 
-exports.signin = function(){
+exports.signin = async function(req, res, next){
+    try {
+        //find a user
+        let user = await db.User.findOne({
+            email: req.body.email
+        });
 
+        let {id, username, profileImageUrl} = user;
+        let isMatch = await user.comparePassword(req.body.password);
+        
+        //verify password
+        //login if password is verified
+        if(isMatch){
+            let token = jwt.sign({
+                id,
+                username,
+                profileImageUrl
+            }, process.env.SECRET_KEY
+            );
+            return res.status(200).json({
+                id,
+                username,
+                profileImageUrl,
+                token
+            });
+        } else {
+            return next({
+                status: 400,
+                message: 'Invalid email/password'
+            })
+        }
+
+    } catch(err){
+        return next({
+            status: 400,
+            message: 'Invalid email/password'
+        })
+    }
 }
 
 exports.signup = async function(req, res, next){
