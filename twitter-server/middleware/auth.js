b804f492cb74b4fd27fa26ac4a81f60939f6d51a
Henry Wang <henry.wang@live.com>
2019-09-03 02:34:28 -0700

    login middleware

--- twitter-server/middleware/auth.js
+++ twitter-server/middleware/auth.js
@@ -2,5 +2,28 @@ require('dotenv').load();
 const jwt = require('jsonwebtoken');
 
 //Authentication - Ensure the user is logged in
+exports.loginRequire = function(req, res, next){
+    try {
+        const token = req.headers.authorization.split('')[1];
+        jwt.verify(token, process.env.SECRET_KEY, function(err, decoded){
+            if(decoded){
+                return next();
+            } else {
+                return next({
+                    status: 401,
+                    message: 'Please log in first'
+                });
+            }
+        });
+    } catch(err){
+        return next({
+            status: 401,
+            message: 'Please log in first'
+        });
+    }
+}
 
 //Authorization Ensure we have the right user
+exports.loginRequire = function(req, res, next){
+    
+}
