19d2bb086de32f332fa8a6987b45c46d3a9bb08f
Henry Wang <henry.wang@live.com>
2019-09-03 04:43:24 -0700

    messages routes

--- twitter-server/models/message.js
+++ twitter-server/models/message.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose');
 const User = require('./user');
 
-const messageSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: true,
-        maxLength: 280
+const messageSchema = new mongoose.Schema(
+    {
+        text: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
     },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+    {
+        timestapms: true
     }
-})
+)
 
 messageSchema.pre('remove', async function(next){
     try {
         //find a user
-        let user = await User.findById(this.userId);
+        let user = await User.findById(this.user);
         //remove the id of the message from their message list
         user.message.remove(this.id);
         //save that user
