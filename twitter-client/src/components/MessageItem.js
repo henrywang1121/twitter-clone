871dc5dbc708716d156f84e2a762d86ea25bbb1d
Henry Wang <henry.wang@live.com>
2019-09-06 01:36:51 -0700

    higher order component

--- twitter-client/src/components/MessageItem.js
+++ twitter-client/src/components/MessageItem.js
@@ -5,23 +5,25 @@ import DefaultProfileImg from '../images/default-profile-image.png';
 
 const MessageItem = ({date, profileImageUrl, text, username}) => (
     <div>
-        <img src={profileImageUrl || DefaultProfileImg}
-            alt={username}
-            height='100'
-            width='100'
-            className='timeline-image'
-        />
-        <div className='message-area'>
-            <Link to='/'>@{username} &nbsp;</Link>
-            <span className='text-muted'>
-                <Moment className='text-muted' format='Do MMM YYYY'>
-                    {date}
-                </Moment>
-            </span>
-            <p>
-                {text}
-            </p>
-        </div>
+        <li className='list-group-item'>
+            <img src={profileImageUrl || DefaultProfileImg}
+                alt={username}
+                height='100'
+                width='100'
+                className='timeline-image'
+            />
+            <div className='message-area'>
+                <Link to='/'>@{username} &nbsp;</Link>
+                <span className='text-muted'>
+                    <Moment className='text-muted' format='Do MMM YYYY'>
+                        {date}
+                    </Moment>
+                </span>
+                <p>
+                    {text}
+                </p>
+            </div>
+        </li>
     </div>
 )
 
--- twitter-client/src/containers/Main.js
+++ twitter-client/src/containers/Main.js
@@ -4,7 +4,8 @@ import {connect} from 'react-redux';
 import Homepage from '../components/Homepage';
 import AuthForm from '../components/AuthForm';
 import {authUser} from '../store/actions/auth';
-import {removeError} from '../store/actions/errors'
+import {removeError} from '../store/actions/errors';
+import withAuth from '../hocs/withAuth';
 
 const Main = props => {
     const {authUser, errors, removeError, currentUser} = props;
@@ -40,6 +41,10 @@ const Main = props => {
                         />
                     )
                 }}/>
+                <Route 
+                    path='/users/:id/messages/new' 
+                    component={withAuth(MessageForm)}
+                />
             </Switch>
         </div>
     )
